cb1b5ca4967dd7d7388abafc162d2b2cd52f50bc
John Huynh <jhnhnyh9@gmail.com>
2018-06-13 11:11:56 -0700

    deploying database into heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-triangular-35111',
       user : '',
       password : '',
       database : 'smart-brain'
